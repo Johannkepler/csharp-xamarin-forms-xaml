a20d1dda695cfe200e97235e5ee89470b9c3dcb9
mui30147 <Maarten.Uijen@ordina.nl>
2020-06-17 08:50:16 +0200

    Completed modul 3

---
+++ XAMLInXamarinForms/XAMLInXamarinForms/Views/Fitness/FitnessView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XAMLInXamarinForms.Views.Fitness
+{
+    public partial class FitnessView : ContentPage
+    {
+        public FitnessView()
+        {
+            InitializeComponent();
+        }
+    }
+}
